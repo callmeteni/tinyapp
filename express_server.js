f59a45c16f7032a8466c0c7534e4c83c3bb63dee
Teniola Ogunlaja <isaacogunlaja9@gmail.com>
2023-06-29 03:18:03 +0000

    Add a POST Route to Receive the Form Submission

--- express_server.js
+++ express_server.js
@@ -30,7 +30,31 @@ app.get("/urls", (req,res) =>{
     res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req,res) =>{
+    res.render("urls_new");
+});
+
 app.get("/urls/:id", (req,res) =>{
     const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
     res.render("urls_show", templateVars);
 });
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/urls", (req,res) =>{
+    console.log(req.body);
+    res.send("okay");
+});
+
+function generateRandomString() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let randomString = '';
+    
+    for (let i = 0; i < 6; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      const randomChar = characters.charAt(randomIndex);
+      randomString += randomChar;
+    }
+    
+    return randomString;
+  }
